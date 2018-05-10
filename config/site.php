e4ae935cc9a7dff58a3db91aa9a3a788e331e0dc
opdss <opdss@qq.com>
2018-05-10 18:32:02 +0800

    update

--- config/site.php
+++ config/site.php
@@ -13,8 +13,10 @@ return array(
 	'description' => '这是一个神奇的网站！！！',
 	//'copyright' => 'Copyright © 2017  istimer.com </br>技术支持：<a href="mailto:wux@tsingning.com" style="color: #2a76fe	;">@阿新</a>',
 	'page_number' => 6,
-	'copyright' => '©2018 <a href="http://www.1tools.net">1tools.net</a>',
-	'icp' => '<a href="http://www.miibeian.gov.cn/">粤ICP备17068889号-3</a>',
+	'copyright' => '©2018 <a href="https://'.$_SERVER['SERVER_NAME'].'">'.$_SERVER['SERVER_NAME'].'</a>',
+	//'copyright' => '©2018 <a href="http://www.1tools.net">1tools.net</a>',
+	//'icp' => '<a href="http://www.miibeian.gov.cn/">粤ICP备17068889号-3</a>',
+	'icp' => '<a href="http://www.miibeian.gov.cn/">粤ICP备88888号</a>',
 	'contact' => '<a href="mailto:opdss@qq.com">阿新</a>',
 	'version' => '0.7.0'
 );
